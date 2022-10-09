78f3df24c435fbc7940ce3c95a1da1a6451e1b9f
Justin Britt <britt@uwindsor.ca>
2022-10-09 14:06:48 -0400

    Added extension methods

---
+++ BepuPhysics.ECS.Components/Extensions/WorldExtensions.cs
@@ -0,0 +1,35 @@
+﻿namespace BepuPhysics.ECS.Components.Extensions
+{
+    using System.Linq;
+
+    using DefaultEcs;
+
+    using BepuPhysics.ECS.Components.Structs;
+
+    public static class WorldExtensions
+    {
+        public static ref SimulationComponent GetSimulationComponentLastRef(
+            this World world)
+        {
+            return ref world
+                .GetEntities()
+                .With<SimulationComponent>()
+                .AsEnumerable()
+                .Where(w => w.IsEnabled() && w.IsAlive)
+                .Last()
+                .Get<SimulationComponent>();
+        }
+
+        public static Simulation GetSimulationLast(
+            this World world)
+        {
+            return world
+                .GetEntities()
+                .With<SimulationComponent>()
+                .AsEnumerable()
+                .Where(w => w.IsEnabled() && w.IsAlive)
+                .Select(w => w.Get<SimulationComponent>().Value)
+                .Last();
+        }
+    }
+}
